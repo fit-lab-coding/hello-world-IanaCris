9359c58c65f76ef13518a60ca480f9e73d78ecfd
IanaCris <ianasousati@gmail.com>
2022-04-05 23:26:37 -0300

    create virtual_network

--- main.tf
+++ main.tf
@@ -17,3 +17,15 @@ resource "azurerm_resource_group" "rg-infra" {
   name     = "infracloudaz"
   location = "eastus"
 }
+
+resource "azurerm_virtual_network" "vnet-infra" {
+  name                = "vnetinfracloud"
+  location            = azurerm_resource_group.rg-infra.location
+  resource_group_name = azurerm_resource_group.rg-infra.name
+  address_space       = ["10.0.0.0/16"]
+
+  tags = {
+    environment = "Production"
+    faculdade = "Impacta"
+  }
+}
