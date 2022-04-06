874884929a5e5c8ecc44c0a824439b44dad4a1f1
IanaCris <ianasousati@gmail.com>
2022-04-05 23:41:17 -0300

    create subnet, publicip and nsg

--- main.tf
+++ main.tf
@@ -29,3 +29,44 @@ resource "azurerm_virtual_network" "vnet-infra" {
     faculdade = "Impacta"
   }
 }
+
+resource "azurerm_subnet" "subnet-infra" {
+  name                 = "subnetinfracloud"
+  resource_group_name  = azurerm_resource_group.rg-infra.name
+  virtual_network_name = azurerm_virtual_network.vnet-infra.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "ip-infra" {
+  name                    = "publicipinfracloud"
+  location                = azurerm_resource_group.rg-infra.location
+  resource_group_name     = azurerm_resource_group.rg-infra.name
+  allocation_method       = "Static"
+
+  tags = {
+    faculdade = "Impacta"
+  }
+}
+
+resource "azurerm_network_security_group" "nsg-infra" {
+  name                = "nsginfracloud"
+  location            = azurerm_resource_group.rg-infra.location
+  resource_group_name = azurerm_resource_group.rg-infra.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+    faculdade = "Impacta"
+  }
+}
